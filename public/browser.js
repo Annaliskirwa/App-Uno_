4b3e3feffd099d482e435a6a665167f81dc8a184
Annaliskirwa <annaliskirwa@gmail.com>
2020-11-30 12:21:08 +0300

    updating db

--- public/browser.js
+++ public/browser.js
@@ -1,1 +1,5 @@
-alert("Hellofrom browser.js")
+document.addEventListener("click", function(e){
+    if (e.target.classList.contains("edit-me")) {
+        alert("Youve clicked the edit button")
+    }
+})
