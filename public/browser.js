a09e6beea521988942b25f9856f75298150fb693
Annaliskirwa <annaliskirwa@gmail.com>
2020-11-30 15:19:09 +0300

    updating user interface aspect

--- public/browser.js
+++ public/browser.js
@@ -1,7 +1,9 @@
 document.addEventListener("click", function(e){
     if (e.target.classList.contains("edit-me")) {
-  let userInput = prompt("Enter your desired new text")
-axios.post('/update-item', {text: userInput, id: e.target.gatAttribute("data-id")}).then(function(){//promise ill be back
+  let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+if (userInput){
+  axios.post('/update-item', {text: userInput, id: e.target.gatAttribute("data-id")}).then(function(){e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
 }).catch(function(){console.log("Try again later")})
     }
+}
 })
