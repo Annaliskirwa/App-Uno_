fd5b7e78f4c52f9b6eff7ec2cbb9a2d5450e8638
Annaliskirwa <annaliskirwa@gmail.com>
2020-11-30 15:42:15 +0300

    user  interface second commit

--- public/browser.js
+++ public/browser.js
@@ -2,8 +2,9 @@ document.addEventListener("click", function(e){
     if (e.target.classList.contains("edit-me")) {
   let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
 if (userInput){
-  axios.post('/update-item', {text: userInput, id: e.target.gatAttribute("data-id")}).then(function(){e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
-}).catch(function(){console.log("Try again later")})
+  axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function(){e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+}).catch(function(){
+  console.log("Try again later")})
     }
 }
 })
