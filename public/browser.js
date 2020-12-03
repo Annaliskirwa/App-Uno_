db2083db61570024d6679536c7f9a41b4282cc0f
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-03 13:28:53 +0300

    js to add a new item

--- public/browser.js
+++ public/browser.js
@@ -6,7 +6,7 @@ document.getElementById("create-form").addEventListener("submit", function(e){
   e.preventDefault()
   axios.post('/create-item', {text: createField.value}).then(function(){
    //Create HTML for a new item
-   alert("You just created a new item")
+   document.getElementById("item-list").insertAdjacentHTML("beforeend", 'hello')
 }).catch(function(){
   console.log("Try again later")})
 })
