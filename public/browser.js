03ac6ba2719930e1fb1ca5a42052fefcd06599c4
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-03 13:42:27 +0300

    including a template

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,13 @@
+function itemTemplate(){
+  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  <span class="item-text">${item.text}</span>
+  <div>
+    <button data-id = "${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+    <button data-id = "${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+  </div>
+</li>`
+}
+
 //Create feature
 let createField = document.getElementById("create-field")
 
@@ -6,7 +16,7 @@ document.getElementById("create-form").addEventListener("submit", function(e){
   e.preventDefault()
   axios.post('/create-item', {text: createField.value}).then(function(){
    //Create HTML for a new item
-   document.getElementById("item-list").insertAdjacentHTML("beforeend", 'hello')
+   document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate())
 }).catch(function(){
   console.log("Try again later")})
 })
